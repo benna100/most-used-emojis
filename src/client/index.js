b0f8c433c1b5a591dedd81a93ed906426c8de039
behuFTP <benjamin.dals.hughes@gmail.com>
2020-04-30 20:15:31 +0200

    improved analysis and added some frontend description

--- src/client/index.js
+++ src/client/index.js
@@ -4,22 +4,31 @@ const $input = document.querySelector("input");
 const $ul = document.querySelector("ul");
 const $span = document.querySelector("span");
 
-document.querySelector("button").addEventListener("click", async () => {
-  $span.classList.add("shown");
-
-  const twitterUsername = $input.value;
-
-  const mostUsedEmojisRequest = await fetch(
-    `api/modules?username=${twitterUsername}`
-  );
-  const mostUsedEmojis = await mostUsedEmojisRequest.json();
+function renderEmojis(emojis) {
   $span.classList.remove("shown");
-  mostUsedEmojis.sort((a, b) => (a.count > b.count ? -1 : 1));
   $ul.innerHTML = "";
-  mostUsedEmojis.forEach(({ emoji, count }) => {
+  emojis.forEach(({ emoji, count }) => {
     const $li = document.createElement("li");
 
     $li.innerHTML = `<span>${emoji}</span><span>${count}</span>`;
     $ul.appendChild($li);
   });
+}
+
+document.querySelector("button").addEventListener("click", async () => {
+  $span.classList.add("shown");
+
+  const twitterUsername = $input.value;
+
+  try {
+    const mostUsedEmojisRequest = await fetch(
+      `api/modules?username=${twitterUsername}`
+    );
+    const mostUsedEmojis = await mostUsedEmojisRequest.json();
+    mostUsedEmojis.sort((a, b) => (a.count > b.count ? -1 : 1));
+    renderEmojis(mostUsedEmojis);
+  } catch (error) {
+    alert(`Could not get Tweets for username: ${twitterUsername}`);
+    $span.classList.remove("shown");
+  }
 });
--- src/server/api/controllers/modules.controller.js
+++ src/server/api/controllers/modules.controller.js
@@ -4,20 +4,43 @@ const HttpError = require("../lib/utils/http-error");
 var request = require("request-promise");
 const extractEmoji = require("extract-emoji");
 
+const getTimelineRequest = async (queryParameterObject) =>
+  request.get("https://api.twitter.com/1.1/statuses/user_timeline.json", {
+    oauth: {
+      consumer_key: process.env.CONSUMER_KEY,
+      consumer_secret: process.env.CONSUMER_SECRET,
+      token: process.env.TOKEN,
+      token_secret: process.env.TOKEN_SECRET,
+    },
+    qs: queryParameterObject,
+  });
+
 async function getTimelineTweets(username) {
-  const timelineTweetsRequest = await request.get(
-    "https://api.twitter.com/1.1/statuses/user_timeline.json",
-    {
-      oauth: {
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        token: process.env.TOKEN,
-        token_secret: process.env.TOKEN_SECRET,
-      },
-      qs: { screen_name: username, count: "200" },
+  const timelineTweetsRaw = await getTimelineRequest({
+    screen_name: username,
+    count: "200",
+    include_rts: "true",
+  });
+
+  let timelineTweets = JSON.parse(timelineTweetsRaw);
+
+  const hasMoreTweets = timelineTweets.length === 200;
+  if (hasMoreTweets) {
+    const timelineTweetsNewRaw = await getTimelineRequest({
+      screen_name: username,
+      count: "200",
+      include_rts: "true",
+      max_id: timelineTweets[timelineTweets.length - 1].id,
+    });
+
+    let timelineTweetsNew = JSON.parse(timelineTweetsNewRaw);
+
+    if (timelineTweetsNew.length > 0) {
+      timelineTweets = timelineTweets.concat(timelineTweetsNew);
     }
-  );
-  return JSON.parse(timelineTweetsRequest);
+  }
+
+  return timelineTweets;
 }
 
 function getEmoijs(tweets) {
@@ -59,9 +82,10 @@ function getMostUsedEmojis(tweets) {
 
 const getModules = async (req) => {
   const timelineTweets = await getTimelineTweets(req.query.username);
-  console.log(req.query.username);
-
-  return getMostUsedEmojis(timelineTweets);
+  const timelineTweetsNoRT = timelineTweets.filter(
+    ({ text }) => !text.includes("RT @")
+  );
+  return getMostUsedEmojis(timelineTweetsNoRT);
 };
 
 module.exports = {
